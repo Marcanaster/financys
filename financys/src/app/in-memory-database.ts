6796a978c92038ba327d2444cb2779de6587222a
Marcos Almeida <marcanaster@gmail.com>
2020-03-28 22:33:33 -0300

    criação do in-memory-web-api

---
+++ financys/src/app/in-memory-database.ts
@@ -0,0 +1,13 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService{
+  createDb(){
+    const categories = [
+      {id: 1, name: 'Lazer', description: 'Cinema, parque, praia ...'},
+      {id: 2, name: 'Serviços', description: 'Mecânico, chaveiro, eletricista...'},
+      {id: 3, name: 'Saúde', description: 'Hospitais, Clínicas, estética...'}
+    ];
+
+    return { categories }
+  }
+}
